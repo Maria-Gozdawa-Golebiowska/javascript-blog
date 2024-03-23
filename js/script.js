212e3e54c50c364256aa44754595e618fe0d529e
Maria Gozdawa-Gołębiowska <m.gozdawa-golebiowska@gmail.com>
2024-03-23 17:19:06 +0100

    my blog article

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 'use strict';
 
 const titleClickHandler = function(event){
+  event.preventDefault();
+
   const clickedElement = this;
   console.log('Link was clicked!');
+
 
   /* [DONE] remove class 'active' from all article links  */
 
@@ -27,9 +30,19 @@ const titleClickHandler = function(event){
 
   /* get 'href' attribute from the clicked link */
 
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
+
+
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
+
   /* add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
+
 }
 
 const links = document.querySelectorAll('.titles a');
